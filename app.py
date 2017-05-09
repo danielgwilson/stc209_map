c8f4e156ce12956ca1521621796e1016cf4e0551
danielgwilson <daniel@danielgwilson.com>
2017-05-09 18:57:09 -0400

    Finished correctly formatted arduino output

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import glob, os
 import random # choosing a key randomly
-# from geopy.geocoders import GoogleV3
+from geopy.geocoders import GoogleV3
 import csv
 import math
 import datetime as dt
@@ -222,17 +222,17 @@ def save_animation():
 
 def save_animation2():
     anim_txt = open('animation.txt', 'w')
+    anim_txt.write('{\n')
     for n in range(0, len(tstamps)):
         # bounds
-        num_buildings = 97
+        num_buildings = 92 # determined by maxi
 
         # init empty pixel array
         pixels = []
-        col = []
-        for i in range(num_buildings):        
-            col.append([0,0,0,0,0])
-            pixels.append(col)
+        for i in range(num_buildings):
+            pixels.append([0,0,0,0,0])
 
+        i = 0
         for index, row in dfms[tstamps[n]].iterrows():
             x = row['x'].astype(int)
             y = row['y'].astype(int)
@@ -240,7 +240,7 @@ def save_animation2():
             if k != 0:
                 ratio = rescale(k)
                 booster = 1.5
-                midpoint = 0.25
+                midpoint = 0.25 * 255
                 print(ratio)
                 if ratio < midpoint:
                     b = ratio * 4 * booster
@@ -250,9 +250,9 @@ def save_animation2():
                     b = 0 * booster
                     g = 0.1 * ratio * booster
                     r = ratio - midpoint * booster
-                pixels[y][x] = [x, y, r, g, b]
-        anim_txt.write("%s\n\n" % pixels)
-
+                pixels[i] = [x, y, recast(r), recast(g), recast(b)]
+                i = i + 1
+        anim_txt.write(("%s,\n" % pixels).replace('[', '{').replace(']','}'))
 
 # def makeTextFile():
     # t = 44
@@ -262,6 +262,10 @@ def save_animation2():
     # for i in range(44):
     #     for j in range(97):
     #         for k in range(5):
+
+# recast value for use in 0-255 array
+def recast(v):
+    return min(int(v), 255)
 
 def main():
     # setup from data
@@ -279,26 +283,26 @@ def main():
 
     # rescale to range function
     from scipy.interpolate import interp1d
-    max_pixel_value = 1.0
+    max_pixel_value = 255
     rescale = interp1d([0.0, max_clients], [0.0, max_pixel_value], bounds_error = False, fill_value = 'extrapolate')
 
-    # save_animation()
-    # for n in range(0, len(tstamps)):
-    # bounds
-    ny, nx = 18, 18
-
-    # init empty pixel array
-    pixels = []
-    for i in range(0, nx):
-        col = []
-        for j in range(0, ny):
-            col.append([0,0.5,0.5])
-        pixels.append(col)
-
-    fig = plt.figure()
-    im = plt.imshow(pixels, interpolation='nearest', animated = True)
-    ani = animation.FuncAnimation(fig, animate, interval = 650)
-    plt.show()
+    save_animation2()
+    # # for n in range(0, len(tstamps)):
+    # # bounds
+    # ny, nx = 18, 18
+    #
+    # # init empty pixel array
+    # pixels = []
+    # for i in range(0, nx):
+    #     col = []
+    #     for j in range(0, ny):
+    #         col.append([0,0.5,0.5])
+    #     pixels.append(col)
+    #
+    # fig = plt.figure()
+    # im = plt.imshow(pixels, interpolation='nearest', animated = True)
+    # ani = animation.FuncAnimation(fig, animate, interval = 650)
+    # plt.show()
 
 if __name__ == "__main__":
     main()
